c870d00d5ca7c66cf5386587ce43bea563d9d018
Sarah Llewelyn <sllewelyn@mdsol.com>
2015-07-03 15:51:36 -0400

    primes

--- enumerable/primes.rb
+++ enumerable/primes.rb
@@ -2,9 +2,24 @@ class PrimeNumbers
     
     def each
         return enum_for(:each) unless block_given?
-        
+        @primes_found = []
+        (2..Float::INFINITY).each do |n|
+            if prime?(n)
+                @primes_found << n
+                yield n
+            end
+        end
     end
     
+    def primes_found
+        @primes_found
+    end
+        
+    private
+    
+    def prime?(num)
+        !primes_found.find { |p| num % p == 0 }
+    end
 end
 
 class AllPositiveNumbers
